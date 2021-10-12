722bf467e1466546ceb86e583a36f5e5c1469e65
Karine <Karine@DESKTOP-2JG4L15>
2021-10-12 19:45:25 +0300

    maybe yes

--- IA_9424.TB.KKY.py
+++ IA_9424.TB.KKY.py
@@ -1,64 +1,108 @@
 import hashlib
 import json
 from time import time
-from flask import Flask, jsonify,request
-from textwrap import dedent
 from uuid import uuid4
+from flask import Flask, jsonify, request
+from textwrap import dedent
 
-class Blockchain (object):
+
+class Blockchain(object):
     def __init__(self):
         self.chain_KKY = []
-        self.current_transactions=[]
-        self.new_block_KKY(previous_hash = 'Khotin', proof = '09122001')
-    def new_block_KKY(self, proof, previous_hash = None):
-        block={
-            'index': len(self.chain_KKY)+1,
-            'timestamp':time(),
-            'transactions': self.current_transactions,
+        self.current_transactions_KKY = []
+        self.new_block_KKY(previous_hash='1', proof=9122001)
+
+    def new_block_KKY(self, proof, previous_hash=None):
+        block_KKY = {
+            'index': len(self.chain_KKY) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions_KKY,
             'proof': proof,
-            'previous_hash': previous_hash or self.hash_KKY(self.chain_KKY[-1])
-            }
-        self.current_transactions=[]
-        self.chain_KKY.append(block)
-        return block
+            'previous_hash': previous_hash or self.hash_KKY(self.chain_KKY[1]),
+        }
+        self.current_transactions_KKY = []
+        self.chain_KKY.append(block_KKY)
+        return block_KKY
 
-    def new_transaction(self, sender,recipient,amount):
-        self.current_transactions.append({
-            'sender':sender,
-            'recipient':recipient,
-            'amount':amount
-            })
-        return self.last_block_KKY['index']+1
+    def new_transaction_KKY(self, sender, recipient, amount):
+        self.current_transactions_KKY.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        print('amount', amount)
+        return self.last_block['index'] + 1
 
-    def last_block_KKY(self):
-        return self.chain_KKY[-1]
-    def hash_KKY(block):
-        block_str=json.dumps(block, sort_keys=True).encode()
-        return hashlib.sha256(block_str).hexdigest()
-
-    def proof_of_work(self,last_proof):
-        proof=0
-        while self.valid_proof(last_proof, proof) is False:
-            proof+=1
-        return proof
+    def proof_of_work_KKY(self, last_proof):
+        proof_KKY = 0
+        while self.valid_proof_KKY(last_proof, proof_KKY) is False:
+            proof_KKY += 1
+            return proof_KKY
 
     @staticmethod
-    def valid_proof(last_proof, proof):
-        guess=f'{last_proof}{proof}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:2]=='01'
+    def valid_proof_KKY(last_proof, proof):
+        guess_KKY = f'{last_proof}{proof}'.encode()
+        guess_hash_KKY = hashlib.sha256(guess_KKY).hexdigest()
+        return guess_hash_KKY[:-2] == "01"
 
-#blockchain=Blockchain()
-#block_KKY=blockchain.new_block_KKY(['09122001'],'acfb0932d7fbb22c2704067702a192ab914c6f30277c0e563fae53c3bc55ab6d')
-#blockchain.proof_of_work(['12'])
-#print(block_KKY)
+    def hash_KKY(self, block):
+        block_string_KKY = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string_KKY).hexdigest()
 
-app=Flask(__name__)
-node_id = str(uuid4()).replace('-','')
-blockchain=Blockchain()
+    @property
+    def last_block(self):
+        return self.chain_KKY[-1]
 
-@app.route("/")
-def index():
-    return "Hello world"
-if __name__=="__main__":
+
+app = Flask(__name__)
+node_id = str(uuid4()).replace('-', '')
+blockchain = Blockchain()
+
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work_KKY(last_proof)
+    blockchain.new_transaction_KKY(
+        sender="0",
+        recipient=node_id,
+        amount=9,
+    )
+
+    previous_hash = blockchain.hash_KKY(last_block)
+    block = blockchain.new_block_KKY(proof, previous_hash)
+    block_hash=blockchain.hash_KKY(block)
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transactions': block['transactions'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+        'hash': block_hash,
+    }
+    return jsonify(response), 200
+
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction_KKY():
+ values = request.get_json()
+ required = ['sender', 'recipient', 'amount']
+ if not all(k in values for k in required):
+     return 'Missing values', 400
+ index = blockchain.new_transaction(values['sender'], values['recipient'], values['amount'])
+ response = {'message': f'Transaction will be added to Block {index}'}
+ return jsonify(response), 201
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain_KKY,
+        'length': len(blockchain.chain_KKY),
+    }
+    return jsonify(response), 200
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
